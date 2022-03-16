57fd2adcdd44d7cb5acee95e7fb6f7e5b0666e81
wsasouza <wsasouza@hotmail.com>
2022-03-16 15:55:11 -0300

    Feat: Implementing Post Card

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,11 @@
 import Head from 'next/head';
+
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-const posts = [
-  { title: 'React Testing', excerpt: 'Learn React Testing' },
-  { title: 'React with Tailwind', excerpt: 'Learn React with Tailwind' },
-];
 
-const Home = () => {
+
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
@@ -15,7 +14,7 @@ const Home = () => {
       </Head>      
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12 ">
         <div className="col-span-1 lg:col-span-8">
-          {posts.map((post) => <PostCard post={post} key={post.title}/>)}
+          {posts.map((post) => <PostCard post={post.node} key={post.node.title}/>)}
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
@@ -28,4 +27,12 @@ const Home = () => {
   );
 };
 
-export default Home;
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: {
+      posts,
+    }
+  }
+}
