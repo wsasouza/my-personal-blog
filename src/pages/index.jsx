fed5007dd0bba6089a2d28fa00374eaa6d8da5a0
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-03-18 18:32:38 -0300

    Test: test connection

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
-        <title>My personal Blog | wsasouza</title>
+        <title>My personal Blog | wsasouza2</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>      
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12 ">
