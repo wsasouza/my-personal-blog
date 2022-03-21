695ae5aa1b7616ca88dcf6bc28bf0e8a0cecbb42
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-03-21 15:21:36 -0300

    Fix: Comments Form

--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -5,41 +5,18 @@ import { submitComment } from '../services'
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false)
   const [localStorage, setLocalStorage] = useState(null)
-  const [showSuccessMessage, setShowSuccessMessage] = useState(true)
-
-  useEffect(() => {
-    setLocalStorage(window.localStorage)
-    const initalFormData = {
-      name: window.localStorage.getItem('name'),
-      email: window.localStorage.getItem('email'),
-      storeData:
-        window.localStorage.getItem('name') ||
-        window.localStorage.getItem('email'),
-    }
-    setFormData(initalFormData)
-  }, [])
-
-  const onInputChange = (e) => {
-    const { target } = e
-    if (target.type === 'checkbox') {
-      setFormData((prevState) => ({
-        ...prevState,
-        [target.name]: target.checked,
-      }))
-    } else {
-      setFormData((prevState) => ({
-        ...prevState,
-        [target.name]: target.value,
-      }))
-    }
-  }
-
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
   const commentEl = useRef()
   const nameEl = useRef()
   const emailEl = useRef()
   const storeDataEl = useRef()
 
-  const handleCommentSubmit = () => {
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('@blog_wsasouza-name')
+    emailEl.current.value = window.localStorage.getItem('@blog_wsasouza-email')
+  }, [])
+
+  const handlePostSubmission = () => {
     setError(false)
 
     const { value: comment } = commentEl.current
@@ -55,29 +32,19 @@ const CommentsForm = ({ slug }) => {
     const commentObj = { name, email, comment, slug }
 
     if (storeData) {
-      localStorage.setItem('@blog_wsasouza-name', name)
-      localStorage.setItem('@blog_wsasouza-email', email)
+      window.localStorage.setItem('@blog_wsasouza-name', name)
+      window.localStorage.setItem('@blog_wsasouza-email', email)
     } else {
-      localStorage.removeItem('@blog_wsasouza-name', name)
-      localStorage.removeItem('@blog_wsasouza-email', email)
+      window.localStorage.removeItem('@blog_wsasouza-name', name)
+      window.localStorage.removeItem('@blog_wsasouza-email', email)
     }
 
     submitComment(commentObj).then((res) => {
-      if (res.createComment) {
-        if (!storeData) {
-          formData.name = ''
-          formData.email = ''
-        }
-        formData.comment = ''
-        setFormData((prevState) => ({
-          ...prevState,
-          ...formData,
-        }))
-        setShowSuccessMessage(true)
-        setTimeout(() => {
-          setShowSuccessMessage(false)
-        }, 3000)
-      }
+      setShowSuccessMessage(true)
+
+      setTimeout(() => {
+        setShowSuccessMessage(false)
+      }, 3000)
     })
   }
 
@@ -88,26 +55,26 @@ const CommentsForm = ({ slug }) => {
       </h3>
       <div className="mb-4 grid grid-cols-1 gap-4">
         <textarea
-          ref={commentEl}
-          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
-          placeholder="Comentário"
           name="comment"
+          ref={commentEl}
+          placeholder="Comentário"
+          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
         />
       </div>
       <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
         <input
+          name="name"
           type="text"
           ref={nameEl}
-          className="w-full rounded-lg bg-gray-100 p-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Nome"
-          name="name"
+          className="w-full rounded-lg bg-gray-100 p-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
         />
         <input
-          type="text"
-          ref={emailEl}
-          className="w-full rounded-lg bg-gray-100 p-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
-          placeholder="E-mail"
           name="email"
+          type="email"
+          ref={emailEl}
+          placeholder="E-mail"
+          className="w-full rounded-lg bg-gray-100 p-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
         />
       </div>
       <div className="mb-4 grid grid-cols-1 gap-4">
@@ -118,7 +85,6 @@ const CommentsForm = ({ slug }) => {
             id="storeData"
             name="storeData"
             value="true"
-            className=""
           />
           <label
             htmlFor="storeData"
@@ -136,7 +102,7 @@ const CommentsForm = ({ slug }) => {
       <div className="mt-8">
         <button
           type="button"
-          onClick={handleCommentSubmit}
+          onClick={handlePostSubmission}
           className="ease inline-block cursor-pointer rounded-full bg-red-500 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:bg-indigo-900"
         >
           Enviar
