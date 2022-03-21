99ccdbba4790a623f6ffb0f7fb868917e56a6944
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-03-20 23:20:08 -0300

    Feat: Implementing Comment Form

--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -1,5 +1,154 @@
-const CommentsForm = () => {
-  return <div>CommentsForm</div>
+import { useRef, useState, useEffect } from 'react'
+
+import { submitComment } from '../services'
+
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null)
+  const [showSuccessMessage, setShowSuccessMessage] = useState(true)
+
+  useEffect(() => {
+    setLocalStorage(window.localStorage)
+    const initalFormData = {
+      name: window.localStorage.getItem('name'),
+      email: window.localStorage.getItem('email'),
+      storeData:
+        window.localStorage.getItem('name') ||
+        window.localStorage.getItem('email'),
+    }
+    setFormData(initalFormData)
+  }, [])
+
+  const onInputChange = (e) => {
+    const { target } = e
+    if (target.type === 'checkbox') {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.checked,
+      }))
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        [target.name]: target.value,
+      }))
+    }
+  }
+
+  const commentEl = useRef()
+  const nameEl = useRef()
+  const emailEl = useRef()
+  const storeDataEl = useRef()
+
+  const handleCommentSubmit = () => {
+    setError(false)
+
+    const { value: comment } = commentEl.current
+    const { value: name } = nameEl.current
+    const { value: email } = emailEl.current
+    const { checked: storeData } = storeDataEl.current
+
+    if (!comment || !name || !email) {
+      setError(true)
+      return
+    }
+
+    const commentObj = { name, email, comment, slug }
+
+    if (storeData) {
+      localStorage.setItem('@blog_wsasouza-name', name)
+      localStorage.setItem('@blog_wsasouza-email', email)
+    } else {
+      localStorage.removeItem('@blog_wsasouza-name', name)
+      localStorage.removeItem('@blog_wsasouza-email', email)
+    }
+
+    submitComment(commentObj).then((res) => {
+      if (res.createComment) {
+        if (!storeData) {
+          formData.name = ''
+          formData.email = ''
+        }
+        formData.comment = ''
+        setFormData((prevState) => ({
+          ...prevState,
+          ...formData,
+        }))
+        setShowSuccessMessage(true)
+        setTimeout(() => {
+          setShowSuccessMessage(false)
+        }, 3000)
+      }
+    })
+  }
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Deixe um comentário
+      </h3>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <textarea
+          ref={commentEl}
+          className="w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Comentário"
+          name="comment"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+        <input
+          type="text"
+          ref={nameEl}
+          className="w-full rounded-lg bg-gray-100 p-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Nome"
+          name="name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="w-full rounded-lg bg-gray-100 p-2 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="E-mail"
+          name="email"
+        />
+      </div>
+      <div className="mb-4 grid grid-cols-1 gap-4">
+        <div>
+          <input
+            ref={storeDataEl}
+            type="checkbox"
+            id="storeData"
+            name="storeData"
+            value="true"
+            className=""
+          />
+          <label
+            htmlFor="storeData"
+            className="ml-2 cursor-pointer text-gray-500"
+          >
+            Salvar meu nome e e-mail para os próximos comentários
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs font-semibold text-red-500">
+          Todos os campos são obrigatórios.
+        </p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmit}
+          className="ease inline-block cursor-pointer rounded-full bg-red-500 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:bg-indigo-900"
+        >
+          Enviar
+        </button>
+        {showSuccessMessage && (
+          <span className="float-right mt-3 text-xl font-semibold text-green-500">
+            Comentário enviado para revisão.
+          </span>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default CommentsForm
