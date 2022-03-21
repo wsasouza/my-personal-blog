e6348735f44e14fecf27940df807412a6525143e
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-03-21 17:58:50 -0300

    Feat: Implementing Comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,5 +1,41 @@
-const Comments = () => {
-  return <div>Comments</div>
+import { useState, useEffect } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
+
+import { getComments } from '../services'
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result))
+  }, [slug])
+
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+          <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+            {comments.length} Comentários
+          </h3>
+          {comments.map((comment) => (
+            <div
+              key={comment.createdAt}
+              className="mb-4 border-b border-gray-100 pb-4"
+            >
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> em{' '}
+                {moment(comment.createdAt).format('DD MMM, YYYY')}
+              </p>
+              <p className="w-full whitespace-pre-line text-gray-600">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Comments
