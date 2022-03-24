3cfc00cbb9c486ff022e4fdd036a9cec478d1041
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-03-24 16:27:16 -0300

    Refactor: Comments Form

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 const Author = ({ author }) => {
   return (
-    <div className="relative mt-20 mb-8 rounded-lg bg-accentColor bg-opacity-20 p-12 text-center">
+    <div className="relative mt-20 mb-8 rounded-lg bg-accentColor bg-opacity-50 p-12 text-center">
       <div className="absolute left-0 right-0 -top-14">
         <Image
           src={author.photo.url}
--- src/components/CommentsForm.jsx
+++ src/components/CommentsForm.jsx
@@ -48,73 +48,80 @@ const CommentsForm = ({ slug }) => {
 
       setTimeout(() => {
         setSubmitted(false)
-      }, 3000)
+      }, 4000)
     })
 
     reset()
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="mb-8 rounded-lg bg-secondary p-8 pb-12 shadow-lg"
-    >
-      <h3 className="mb-8 border-b pb-4 text-xl font-semibold text-primary">
-        Deixe um comentário
-      </h3>
-      <div className="mb-4 grid grid-cols-1 gap-4">
-        <textarea
-          {...register('comment', { required: true })}
-          error={errors.comment}
-          placeholder="Comentário"
-          rows={2}
-          className="focus:ring- w-full rounded-lg bg-panel p-4 text-lightGray outline-none focus:ring-1 focus:ring-yellow-500 focus:ring-opacity-50"
-        />
-        {errors.comment && (
-          <span className="text-red-600">{errors.comment.message}</span>
-        )}
-      </div>
-      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <div className="flex flex-col">
-          <input
-            {...register('name', { required: true })}
-            type="text"
-            error={errors.name}
-            placeholder="Nome"
-            className="w-full rounded-lg bg-panel p-2 text-lightGray outline-none focus:ring-1 focus:ring-yellow-500 focus:ring-opacity-50"
-          />
-          {errors.name && (
-            <span className="mt-2 text-red-600">{errors.name.message}</span>
-          )}
+    <>
+      {submitted ? (
+        <div className="mx-auto mb-8 w-full rounded-lg bg-accentColor p-8 pb-12 shadow-lg">
+          <h3 className="text-3xl font-bold text-black">
+            Obrigado por enviar um comentário :)
+          </h3>
+          <p>Assim que ele for aprovado, vai ser listado abaixo.</p>
         </div>
-        <div className="flex flex-col">
-          <input
-            {...register('email', { required: true })}
-            type="email"
-            error={errors.email}
-            placeholder="E-mail"
-            className="w-full rounded-lg bg-panel p-2 text-lightGray outline-none focus:ring-1 focus:ring-yellow-500 focus:ring-opacity-50"
-          />
-          {errors.email && (
-            <span className="mt-2 text-red-600">{errors.email.message}</span>
-          )}
-        </div>
-      </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="mb-8 rounded-lg bg-secondary p-8 pb-12 shadow-lg"
+        >
+          <h3 className="mb-8 border-b pb-4 text-xl font-semibold text-primary">
+            Deixe um comentário
+          </h3>
+          <div className="mb-4 grid grid-cols-1 gap-4">
+            <textarea
+              {...register('comment', { required: true })}
+              error={errors.comment}
+              placeholder="Comentário"
+              rows={2}
+              className="focus:ring- w-full rounded-lg bg-panel p-4 text-lightGray outline-none focus:ring-1 focus:ring-yellow-500 focus:ring-opacity-50"
+            />
+            {errors.comment && (
+              <span className="text-red-600">{errors.comment.message}</span>
+            )}
+          </div>
+          <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+            <div className="flex flex-col">
+              <input
+                {...register('name', { required: true })}
+                type="text"
+                error={errors.name}
+                placeholder="Nome"
+                className="w-full rounded-lg bg-panel p-2 text-lightGray outline-none focus:ring-1 focus:ring-yellow-500 focus:ring-opacity-50"
+              />
+              {errors.name && (
+                <span className="mt-2 text-red-600">{errors.name.message}</span>
+              )}
+            </div>
+            <div className="flex flex-col">
+              <input
+                {...register('email', { required: true })}
+                type="email"
+                error={errors.email}
+                placeholder="E-mail"
+                className="w-full rounded-lg bg-panel p-2 text-lightGray outline-none focus:ring-1 focus:ring-yellow-500 focus:ring-opacity-50"
+              />
+              {errors.email && (
+                <span className="mt-2 text-red-600">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+          </div>
 
-      <div className="mt-4">
-        <input
-          type="submit"
-          value="Enviar"
-          className="ease inline-block cursor-pointer rounded-full bg-accentColor px-8 py-3 text-lg font-medium text-black transition duration-500 hover:bg-opacity-70 hover:text-white"
-        />
-
-        {submitted && (
-          <span className="float-right mt-3 text-xl font-semibold text-green-500">
-            Comentário enviado para revisão.
-          </span>
-        )}
-      </div>
-    </form>
+          <div className="mt-4">
+            <input
+              type="submit"
+              value="Enviar"
+              className="ease inline-block cursor-pointer rounded-full bg-accentColor px-8 py-3 text-lg font-medium text-black transition duration-500 hover:bg-opacity-95 hover:font-semibold"
+            />
+          </div>
+        </form>
+      )}
+    </>
   )
 }
 
